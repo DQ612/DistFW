78a3f477ac0ec5664e5a361b743cd5c040247f3f
Wei Dai <daiwei89@gmail.com>
2014-04-25 17:52:17 -0400

    skeleton code

---
+++ src/svm_fw/BCFWSolver.hpp
@@ -0,0 +1,15 @@
+// Author: Dai Wei (wdai@cs.cmu.edu)
+// Date: 2014.04.25
+
+#include "svm_oracle/abstract_svm_oracle.hpp"
+
+namespace fw {
+
+class BCFWSolver {
+public:
+
+private:
+
+};
+
+}  // namespace fw
