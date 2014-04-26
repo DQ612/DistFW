245321efbabf40834ba34c3797163cd8fe685333
veeranjaneyulus <veeranjaneyulus@gmail.com>
2014-04-26 00:23:46 -0400

    add chain loss

--- src/chain_oracle.cpp
+++ src/chain_oracle.cpp
@@ -1,5 +1,25 @@
 #include "chain_oracle.hpp"
 
 using namespace fw;
+using namespace std;
 
+Feature ChainOracle::GenerateFeatureMap(const Feature& x, const Feature& y){
+    return Feature();
+}
 
+double ChainOracle::Loss(const Feature& y_truth, const Feature& y_predict){
+	
+	double loss = 0;
+	Feature::const_iterator t = y_truth.begin();
+	Feature::const_iterator p = y_predict.begin();
+	for(; t != y_truth.end(); t++, p++ ){
+		if( *t != *p )
+			loss += 1;
+	}
+	return loss / y_truth.size();
+}
+
+Feature ChainOracle::MaxOracle(const Feature& w, const Feature& x_i,
+        const Feature& y_i, int num_y_states){
+    throw std::string("MaxOracle not implemented");
+}
--- src/chain_oracle.hpp
+++ src/chain_oracle.hpp
@@ -1,6 +1,8 @@
 // Author: Veeru Sadhanala (vsadhana@cs.cmu.edu)
 // Date: 2014.04.25
 
+#ifndef FW_CHAINORACLE_HPP
+#define FW_CHAINORACLE_HPP
 
 #include "abstract_svm_oracle.hpp"
 
@@ -12,14 +14,12 @@ class ChainOracle : public AbstractSVMOracle {
 
         double Loss(const Feature& y_truth, const Feature& y_predict);
 
-        Feature MaxOracle(const Feature& w, const Feature& x_i, 
+        Feature MaxOracle(const Feature& w, const Feature& x_i,
                 const Feature& y_i, int num_y_states);
 
-
-
-
-
+        ChainOracle(){}
 
 };
 
 }
+#endif
