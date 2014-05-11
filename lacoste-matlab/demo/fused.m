f4742bf37ab87092e6402fd804a957d31baaea48
willie neiswanger <willie.neiswanger@gmail.com>
2014-05-10 21:16:04 -0400

    Slight modification to fused.m, and added Illustration_fused.png, both in lacoste-matlab/demo/ dir.

--- lacoste-matlab/demo/fused.m
+++ lacoste-matlab/demo/fused.m
@@ -1,4 +1,4 @@
-
+addpath(genpath('../solvers'))
 
 s = RandStream.create('mt19937ar','seed',5494);
 RandStream.setGlobalStream(s);
@@ -28,30 +28,55 @@ options.num_passes = 1000000; % max number of passes through data
 
 %taus = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377];
 %taus = [1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-taus = [1, 70];
-%taus = 1;
+%taus = [1, 70];
+taus = [1,10,20,30,40,50,60,70];
+
 times = zeros(size(taus));
 epochs = zeros(size(taus));
-for i=1:numel(taus)
-    options.tau = taus(i)/n;
-    options.debug_iter = n/taus(i);
-    
-    [stats] = solverFWFused( Y, lambda, options );
-    
-    times(i) = stats.time;
-    epochs(i) = stats.k;
-    
+
+
+numrepeats = 5;
+for j=1:numrepeats
+    for i=1:numel(taus)
+        options.tau = taus(i)/n;
+        options.debug_iter = n/taus(i);
+        
+        [stats] = solverFWFused( Y, lambda, options );
+        
+        %times(i) = stats.time;
+        %epochs(i) = stats.k;
+        rep_times(j,i) = stats.time;
+        rep_epochs(j,i) = stats.k;
+    end
 end
 
-%%
 figure
 plot(taus(1:end), epochs(1:end),'LineWidth', 2)
 xlabel('\tau','FontSize', 16);
 ylabel('\tau Epochs', 'FontSize', 16);
 title('Variation of number of epochs with \tau','FontSize', 16);
 
+%%%%
+for j=1:numrepeats
+    rep_speedups(j,:) = rep_epochs(j,1) ./ rep_epochs(j,:);
+end
+for i=1:size(rep_epochs,2)
+    speedups(i) = mean(rep_speedups(:,i));
+    speedups_err(i) = std(rep_speedups(:,i));
+end
+
 figure
-speedups = epochs(1) ./ epochs;
+errorbar(taus(1:end), speedups(1:end), speedups_err(1:end),'b')
+hold on, plot(1:1:taus(end), 1:1:taus(end), 'r');
+xlim([0,taus(end)])
+xlabel('\tau','FontSize', 16);
+ylabel(' Speedup', 'FontSize', 16);
+title('Speedup with distribution','FontSize', 16);
+legend('y=x line', 'speedup')
+%%%%
+
+
+figure
 plot(1:1:taus(end), 1:1:taus(end), 'r', taus(1:end), speedups(1:end),'LineWidth', 2)
 xlabel('\tau','FontSize', 16);
 ylabel(' Speedup', 'FontSize', 16);
